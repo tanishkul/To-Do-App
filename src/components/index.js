aff07331d43f3115d00d969e47283c39432bfb7f
tanishkul <tanishkul@gmail.com>
2021-06-13 22:10:58 +0530

    Refactored code

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,3 @@
 export { default as Card } from './Card/Card';
 export { default as Modal } from './Modal/Modal';
 export { default as Form } from './Form/Form';
-export { default as Container } from './Container/Container';
-export { default as TriggerButton } from './TriggerButton/TriggerButton';
