d9fcbf2631306192acdee80f3fbae3dda9df061d
tanishkul <tanishkul@gmail.com>
2021-06-24 19:57:16 +0530

    Changes in routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import './App.css';
 import React from 'react';
-import {
-  BrowserRouter as Router, Route, Switch, Redirect
-} from 'react-router-dom';
 
 import { TodoList } from './modules';
 
-class App extends React.Component {
-
-  render() {
+const App = () => {
     return (
-      <Router>
-        <Switch>
-          <Route component={TodoList} exact path="/" />
-          <Redirect from="*" to="/" />
-        </Switch>
-      </Router>
+      <TodoList />
     );
-  }
 }
 
 export { App };
